a2bde524742a1e72e9a88311591faede2037ad06
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 11:45:20 +0900

    추가수정

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # streamlit_test
 ## 쿠쉬쿠쉬의 역사
 * 오리온, 페스츄리형 크래커 '쿠쉬쿠쉬' 출시
-[기사 전문](기사링크)
+[기사 전문](http://www.100ssd.co.kr/news/articleView.html?idxno=51288)
---
+++ frist_webapp/app.py
@@ -0,0 +1,16 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 제 첫 웹페이지 입니다
+    ## 부족하지만 많이 사랑해주세요!
+    * 1$ = 1,300
+    """
+)
+
+#https://unsplash.com/
+st.image(
+
+)
