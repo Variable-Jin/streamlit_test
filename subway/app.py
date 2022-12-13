e98889f333b2d55db00fe7be2f3c21b592631c18
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 18:34:42 +0900

    ㅇ

--- subway/app.py
+++ subway/app.py
@@ -11,5 +11,5 @@ df = pd.read_csv('./subway/ssubway.csv', encoding='CP949')
 st.write(df)
 
 fig = plt.figure(figsize=(10,7))
-sns.histplot(data=df, x='호선', hue='조사일자' ,bin=10)
+sns.histplot(data=df, x='호선', hue='조사일자', bin=10)
 st.pyplot(fig)
