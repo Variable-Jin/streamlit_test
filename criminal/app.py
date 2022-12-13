62649e5972a0ce358e58d0177a6ccc4437353ce0
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 16:23:32 +0900

    1234

---
+++ criminal/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.write(
+   "https://www.data.go.kr/data/15069309/fileData.do"
+)
+
+df = pd.read_csv('./criminal/crime.csv')
+st.write(df)
+
