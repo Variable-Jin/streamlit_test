8c6b62ad376f9ef29eede37040f6eabe2124ca97
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 12:39:52 +0900

    .

--- frist_webapp/app.py
+++ frist_webapp/app.py
@@ -10,7 +10,7 @@ st.write(
     """
 )
 
-#https://unsplash.com/
+#https://pixabay.com/kost.image
 st.image(
-
+    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
 )
---
+++ frist_webapp/lucky/app.py
@@ -0,0 +1,20 @@
+# streamlit 라이브러리 호출
+import streamlit as streamlit
+import numpy as numpy
+
+# https://docs.streamlit.io/library/get-started/main-concepts
+# st.write() 마크다운
+st.title("조추첨 메시지")
+st.header("여러분의 참여를 환영합니다!")
+
+# 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
+# 3 x 4 (row, col)
+# 열을 배치하는 메소드
+colunbs = st.colums(4)
+# col1, col2, col3, col4
+for idx, c in enumerate(columns):
+    st.text_input("조 추첨 대상")
+# 13명이 소속될 조 이름을 넣을 위치
+
+# <추첨 버튼>
+# 13개의 짝을 지어서 표시해줄 그래픽
