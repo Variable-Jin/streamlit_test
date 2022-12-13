8f09a29f44effd2b9ecfba13c7ce44c2d16173c6
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 14:19:31 +0900

    1

--- frist_webapp/lucky/app.py
+++ frist_webapp/lucky/app.py
@@ -10,7 +10,10 @@ st.header("여러분의 참여를 환영합니다!")
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 4 x 4 (row, col)
 # 열을 배치하는 메소드
-# st.columns(n) : n만큼의 컬럼 리스트를 생성
+# x = st.columns(n) : n만큼의 컬럼 리스트를 생성
+
+tabs = st.tabs(['참가자'])
+
 columns = st.columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
@@ -21,7 +24,7 @@ for idx, col in enumerate(columns):  # 열의 위치
         # key가 겹치면 안 됨
         # col 안에 메소드를 통해서 요소들을 생성해주겠다
         col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", 
-        key=f"{idx+1 + idx2 * 4}"
+        key=f"n{idx+1 + idx2 * 4}"
         )  # 4번 호출됨
 # 13명이 소속될 조 이름을 넣을 위치
 st.write(st.session_state)
