d19d2029b64a43730ec1e1a1ff232147e005fa3a
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 14:58:25 +0900

    123

--- frist_webapp/lucky/app.py
+++ frist_webapp/lucky/app.py
@@ -3,6 +3,7 @@
 # https://docs.streamlit.io/library/cheatsheet
 import streamlit as st  # st라는 이름으로 사용
 import numpy as np
+import pandas as pd
 # st.write() 마크다운
 st.title("조추첨 페이지")
 st.header("여러분의 참여를 환영합니다!")
@@ -29,21 +30,82 @@ for idx, col in enumerate(columns):  # 열의 위치
         )  # 4번 호출됨
 # 2번째 : 조
 # columns -> columns2, tabs[0] -> tabs[1]
-columns = tabs[1].columns(4) # 화면을 열로 나누어서 배치
+columns2 = tabs[1].columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
 # enumerate : index, value 묶음
 # columns -> columns2
-for idx, col in enumerate(columns):  # 열의 위치
-    # col.text_input(f"조 목록 {idx+1}", key=idx)
+for idx, col in enumerate(columns2): # 열의 위치
+    # 이중 for문 (for문 안에 for문)
+    # col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
     for idx2 in range(4):
         # key가 겹치면 안 됨
         # col 안에 메소드를 통해서 요소들을 생성해주겠다
-        col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", 
-        key=f"n{idx+1 + idx2 * 4}"
-        )  # 4번 호출됨
-
+        col.text_input(
+            f"조 목록 {idx+1 + idx2 * 4}",
+            key=f"g{idx+1 + idx2 * 4}" # n -> g => 겹치지 말라고
+        ) # 4번 호출됨'
+        
 # 13명이 소속될 조 이름을 넣을 위치
 st.write(st.session_state)
+   # np.random.choice -> 추출해서 이름들, 목록
+   # 1. st.session_state - n, g가 섞여있음
+   ss = pd.Series(st.session_state)   # 딕셔너리 ->
+   # st.write(ss)
+   # ss2 = ss[ss ! ""]
+   ss2 = ss[ss.ne("")]
+   n_data = ss2[ss2]
+   # str : string 관련된 메소드를 사용할 수 있게 함
+   n_idx = ss2.index.str.contains('n')
+   n_data = ss2[n_idx]
+   # st.write(n_data)
+
+   g_idx = ss2.index.str.contains('g')
+   g_data = ss2[g_idx]
+   # st.write(g_data)
+
+   # n_data를 섞어줄 것임 (비복원으로)
+   n_rd = np.random.choice(n_data, len(n_data), replace=False)
+   st.write(n_rd)
+   g_rd = np.random.choice(g_data, len(g_data), replace=False)
+   st.write(g_rd)
+
+   # 2. df 형태로 정리
+   df = pd.DataFrame({
+    "추첨 대상자 이름": n_rd,
+    "조 이름": g_rd,
+})
+st.write(df)
 # <추첨 버튼>
-# 13개의 짝을 지어서 표시해줄 그래픽
+   if st.button('추첨 시작!'): 
+   # 13명이 소속될 조 이름을 넣을 위치
+   st.write(st.session_state)
+   # np.random.choice -> 추출해서 이름들, 목록
+   # 1. st.session_state - n, g가 섞여있음
+ss = pd.Series(st.session_state)   # 딕셔너리 ->
+   # st.write(ss)
+   # ss2 = ss[ss ! ""]
+ss2 = ss[ss.ne("")]
+n_data = ss2[ss2]
+# str : string 관련된 메소드를 사용할 수 있게 함
+n_idx = ss2.index.str.contains('n')
+n_data = ss2[n_idx]
+# st.write(n_data)
+
+g_idx = ss2.index.str.contains('g')
+g_data = ss2[g_idx]
+# st.write(g_data)
+
+# n_data를 섞어줄 것임 (비복원으로)
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
+st.write(n_rd)
+g_rd = np.random.choice(g_data, len(g_data), replace=False)
+st.write(g_rd)
+
+# 2. df 형태로 정리
+df = pd.DataFrame({
+    "추첨 대상자 이름": n_rd,
+    "조 이름": g_rd,
+})
+st.balloons()
+st.write(df)
