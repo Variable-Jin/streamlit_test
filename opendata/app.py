06aa00f0240df134eba167338b79fdb93bed4fc3
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-13 16:06:56 +0900

    1234

--- opendata/app.py
+++ opendata/app.py
@@ -1,4 +1,13 @@
-# streamlit 라이브러리 호출
-import streamlit as st 
+import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.write(
+   "https://www.data.go.kr/data/15070282/fileData.do"
+)
+# UTF-8 / CP-949
+# https://seong6496.tistory.com/269
+df = pd.read_csv('./opendata/data.csv', encoding='cp949')
+st.write(df)
+https://github.com/qus0in/streamlit_test/tree/main/opendata
