f6cb5a26fb4e470b1d747e0ad11ed03d41a5903c
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-14 11:19:30 +0900

    f

--- subway/app.py
+++ subway/app.py
@@ -4,23 +4,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 st.write(
-   "https://www.data.go.kr/data/15081069/fileData.do"
+   "https://www.naver.com/"
 )
-df = pd.read_csv('./cafe/cafe.csv', encoding='CP949')
+df = pd.read_csv('./subway/subway.csv', encoding='CP949')
 st.write(df)
 
-from glob import glob
+fig = plt.figure(figsize=(10,4))
+sns.histplot(data=df, x='호선', hue='조사일자', multiple='stack')
+st.pyplot(fig)
 
-file_names = glob('./cafe/cafe.csv')
-total = pd.DataFrame()
+fig2 = plt.figure(figsize=(10,4))
+sns.kdeplot(data=df, x='호선')
+st.pyplot(fig2)
 
-for file_name in file_names:
-   temp = pd.read_csv(file_name, encoding='cp949')
-   total = pd.concat([total, temp])
-
-total.reset_index(inplace=True, drop=True)
-st.write(total)
+fig3 = plt.figure(figsize=(10,4))
+sns.kdeplot(data=df, x='호선', hue='조사일자', multiple='stack')
+st.pyplot(fig3)
 
 df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='sum')
 fp = df.pivot_table(index='호선', columns='구분', values='5시30분', aggfunc='sum')
