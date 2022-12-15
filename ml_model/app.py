40c26ec69fe98b158e0e1a57a5be9a5bbc8e3c89
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-15 17:12:40 +0900

    sd

--- ml_model/app.py
+++ ml_model/app.py
@@ -1,10 +1,6 @@
 import streamlit as st
-import pandas as pandas
-
+import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
-
-import joblib
-model = joblib.load('./ml_model/app.py')
-model_info = pd.Series(model_from_joblib.coef_, index = X.columns)
-st.write(model_info)
+5:07
+pd.Series(model.coef_, index = X.columns)
