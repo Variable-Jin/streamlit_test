ac6688ab2ae980222d588304d41353eb536dc00b
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-15 17:08:33 +0900

    sd

--- ml_model/app.py
+++ ml_model/app.py
@@ -4,4 +4,7 @@ import pandas as pandas
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
 
-
+import joblib
+model = joblib.load('./ml_model/app.py')
+model_info = pd.Series(model_from_joblib.coef_, index = X.columns)
+st.write(model_info)
