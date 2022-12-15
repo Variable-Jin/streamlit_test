fa097f299d2101dd1f8991198bd1746901ae4830
ooingsj <120434509+usersoojin@users.noreply.github.com>
2022-12-15 17:12:48 +0900

    x

--- ml_model/app.py
+++ ml_model/app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
-5:07
+
 pd.Series(model.coef_, index = X.columns)
